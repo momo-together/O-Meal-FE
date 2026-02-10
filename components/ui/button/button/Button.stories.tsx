eedd0cb8d3b96254418512d1fc1ac65dd37fe96a
hanheel <hanhee630630@gmail.com>
2026-02-10 19:22:17 +0900

    test: Button 컴포넌트 스토리북 제작

---
+++ components/ui/button/button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "./Button";
+import LikeIcon from "@/assets/icons/like.svg?react";
+
+const meta: Meta<typeof Button> = {
+  title: "UI/Button/Button",
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Button {...args}>
+      <LikeIcon />
+    </Button>
+  ),
+};
