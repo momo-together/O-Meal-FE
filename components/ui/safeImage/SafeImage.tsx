3197dbe51e7e732366ba2aa9b226e9854128b3cc
hanheel <hanhee630630@gmail.com>
2026-02-14 04:00:47 +0900

    refactor: SafeImage에 fallback 기본값 제거

--- components/ui/safeImage/SafeImage.tsx
+++ components/ui/safeImage/SafeImage.tsx
@@ -2,17 +2,16 @@
 
 import Image, { type ImageProps } from "next/image";
 import { useState } from "react";
-import { FALLBACK_IMAGE } from "@/constants/image";
 
 interface SafeImageProps extends ImageProps {
   src: string;
   fallback: string;
 }
 
-const SafeImage = ({ src, fallback = "/images/no-image.png", ...props }: SafeImageProps) => {
+const SafeImage = ({ src, fallback, ...props }: SafeImageProps) => {
   const [isError, setIsError] = useState(false);
 
-  return <Image src={isError ? fallback : src} onError={() => setIsError(true)} {...props} placeholder="blur" blurDataURL={FALLBACK_IMAGE} />;
+  return <Image src={isError ? fallback : src} onError={() => setIsError(true)} {...props} />;
 };
 
 export default SafeImage;
