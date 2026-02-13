c32204c6a46c6134136bec8ca164d34fb607c833
hanheel <hanhee630630@gmail.com>
2026-02-14 01:28:33 +0900

    style: UI 크기를 작게 수정

---
+++ app/(tabs)/mypage/_components/profileInput/ProfileInput.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import ImageInput from "@/components/ui/input/imageInput/ImageInput";
+
+const ProfileInput = () => {
+  const [_, setImage] = useState<File | null>(null);
+  const submitImage = (image: File) => {
+    setImage(image);
+    // TODO : 서버에 전송하는 로직
+  };
+  return <ImageInput id="profile-image" onImageChange={() => submitImage} defaultImage="/fallback.png" />;
+};
+
+export default ProfileInput;
