73bbba8da94b806093c2d5aa10d592da84e55401
hanheel <hanhee630630@gmail.com>
2026-02-13 22:49:29 +0900

    feat: fallbackRoute를 명시적으로 지정할 수 있도록 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/layout/header/Header";
 
 export default function Home() {
-  return <Header title="숙명여자대학교" />;
+  return <Header title="숙명여자대학교" fallbackRoute="/" />;
 }
