4ab564cab1f9ea6b2053764b6b03a9560d108d8c
hanheel <hanhee630630@gmail.com>
2026-02-14 01:27:58 +0900

    feat: Tabmenu 레이아웃 구현

---
+++ app/(tabs)/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Tabmenu from "@/components/ui/tabmenu/Tabmenu";
+
+const TabLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const selectedTab = pathname.startsWith("/mypage") ? "my" : "home";
+
+  return (
+    <div className="flex min-h-dvh flex-col">
+      <main className="flex-1">{children}</main>
+      <div className="sticky bottom-0">
+        <Tabmenu selectedTab={selectedTab} />
+      </div>
+    </div>
+  );
+};
+
+export default TabLayout;
