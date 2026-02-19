8182416fb1e857b13d1d5e2d80aecdd1980d3619
hanheel <hanhee630630@gmail.com>
2026-02-20 01:06:06 +0900

    fix: HashtagButton 의 props를 Button 속성을 상속하도록 수정

--- components/specific/hashtag/hashtagButton/HashtagButton.tsx
+++ components/specific/hashtag/hashtagButton/HashtagButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import clsx from "clsx";
+import type { ButtonHTMLAttributes } from "react";
 
-interface HashtagButtonProps {
+interface HashtagButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isSelected: boolean;
   text: string;
 }
 
-const HashtagButton = ({ isSelected, text }: HashtagButtonProps) => {
+const HashtagButton = ({ isSelected, text, ...restProps }: HashtagButtonProps) => {
   return (
     <button
       type="button"
@@ -15,6 +16,7 @@ const HashtagButton = ({ isSelected, text }: HashtagButtonProps) => {
         "bg-bg-white inline-flex py-1 px-3 justify-center items-center rounded-full select-none",
         isSelected ? "border border-primary-point" : "border border-gray-300",
       )}
+      {...restProps}
     >
       <span className={clsx("typo-caption", isSelected ? "text-primary-point" : "text-gray-500")}>{text}</span>
     </button>
