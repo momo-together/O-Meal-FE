ee44c063ff99f63d67aff3c8ebc290f2b29538fd
hanheel <hanhee630630@gmail.com>
2026-03-07 22:03:21 +0900

    chore: 개행 변경 및 불필요한 파일 삭제

--- components/ui/imageGrid/ImageGrid.tsx
+++ components/ui/imageGrid/ImageGrid.tsx
@@ -13,9 +13,18 @@ const ImageGrid = ({ imageInfos }: ImageGridProps) => {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(80px,1fr))] gap-2 px-4">
       {imageInfos.map(({ src, alt }, index) => (
-        //biome-ignore lint/suspicious/noArrayIndexKey: src와 index의 조합을 사용
-        <div key={`${src}-${index}`} className="relative aspect-square overflow-hidden rounded-xl">
-          <Image fill sizes="110px" src={src} alt={alt} className="object-cover" />
+        <div
+          //biome-ignore lint/suspicious/noArrayIndexKey: src와 index의 조합을 사용
+          key={`${src}-${index}`}
+          className="relative aspect-square overflow-hidden rounded-xl"
+        >
+          <Image
+            fill
+            sizes="110px"
+            src={src}
+            alt={alt}
+            className="object-cover"
+          />
         </div>
       ))}
     </div>
